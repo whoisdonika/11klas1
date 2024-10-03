2b7a9b57bb896796e5243aa436fdb28ecde8d7cc
Admin <Admin@DESKTOP-4RHGVT9>
2024-10-03 20:11:51 +0300

    Add project files.

---
+++ FigureCalculations/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigureCalculations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many rectangles would you like to add? - ");
+            int rectangleCount = int.Parse(Console.ReadLine());
+
+            List<Rectangle> rectangles = new List<Rectangle>();
+
+            for (int i = 1; i <= rectangleCount; i++)
+            {
+                Console.WriteLine($"Enter side A and B for rectangle number {i} : ");
+                int sideA = int.Parse(Console.ReadLine());
+                int sideB = int.Parse(Console.ReadLine());
+
+                if (sideA < 1)
+                {
+                    Console.Write("Invalid value for side A. Try again : ");
+                    sideA = int.Parse(Console.ReadLine());
+                }
+
+                if (sideB < 1)
+                {
+                    Console.Write("Invalid value for side B. Try again : ");
+                    sideB = int.Parse(Console.ReadLine());
+                }
+
+
+                Rectangle rectangle = new Rectangle(sideA, sideB);
+
+                rectangles.Add(rectangle);
+            }
+
+            foreach (Rectangle r in rectangles)
+            {
+                Console.WriteLine($"This rectangle's perimeter is {r.CalculatePerimeter()}cm and its area is {r.CalculateArea()}cm2.");
+            }
+        }
+    }
+}
---
+++ FigureCalculations/Rectangle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FigureCalculations
+{
+    internal class Rectangle
+    {
+		private int sideA;
+
+		public int SideA
+		{
+			get { return sideA; }
+			set { sideA = value; }
+		}
+
+		private int sideB;
+
+		public int SideB
+		{
+			get { return sideB; }
+			set { sideB = value; }
+		}
+		public Rectangle(int sideA, int sideB)
+		{
+			this.sideA = sideA;
+			this.sideB = sideB;
+		}
+		public int CalculateArea()
+		{
+			return this.SideA * this.SideB;
+		}
+
+		public int CalculatePerimeter()
+		{
+			return 2 * (this.SideA + this.SideB);
+		}
+
+
+	}
+}
